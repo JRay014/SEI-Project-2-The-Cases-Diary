92f84e4fa9e6fab589451e83c7fb06268d52125f
John Raymond <jray014@gmail.com>
2020-08-12 18:12:18 -0400

    routes initial done

---
+++ routes/auth-router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth-services/local');
+
+const authHelpers = require('../services/auth-services/auth-helpers');
+
+authRouter.get('/login', authHelpers.loginRedirect, (req, res) => {
+  res.render('/index');
+});
+authRouter.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/index',
+    failureFlash: true,
+  })
+);
+
+authRouter.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('back');
+});
+
+module.exports = authRouter;
+
--- routes/cases-router.js
+++ routes/cases-router.js
@@ -5,4 +5,23 @@ const casesController = require('../controllers/cases-controller');
 
 caseRouter.get('/', casesController.index);
 caseRouter.post('/', casesController.create);
+caseRouter.get('/new', (req, res) => {
+    res.render('cases/new');
+});
 
+caseRouter.get('/:id([0-9]+)', casesController.show, (req, res) => {
+    res.render('cases/show', {
+        certainCase: res.locals.certainCase,
+    })
+});
+
+caseRouter.get('/:id([0-9]+)/edit', casesController.show, (req, res) => {
+    res.render('cases/edit', {
+        certainCase: res.locals.certainCase,
+    })
+});
+
+caseRouter.put('/:id([0-9]+)', casesController.update);
+caseRouter.delete('/:id([0-9]+)', casesController.delete);
+
+module.exports = caseRouter;
